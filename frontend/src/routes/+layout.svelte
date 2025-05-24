a106c9d7d714ad4ad746bca7ac565a9a4ba8a7df
William Y. Feng <womogenes2@gmail.com>
2025-05-24 17:15:45 -0400

    Add minimal landing page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,4 +4,10 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<svelte:head>
+	<title>Stationers</title>
+</svelte:head>
+
+<div class="h-screen p-4">
+	{@render children()}
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-full flex-col items-center justify-center gap-2">
+	<h1 class="text-4xl font-bold">Stationers</h1>
+	<p>A game about bookshops</p>
+</div>
