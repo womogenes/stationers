ce65322f82717ce6debdf074ae600e76cd1f9583
William Y. Feng <womogenes2@gmail.com>
2025-05-25 14:02:39 -0400

    Make buttons pretty

---
+++ frontend/src/lib/game/game.svelte.ts
@@ -0,0 +1,12 @@
+// Represents state for a game
+// Methods for actions, serialization, etc.
+
+import './bookshop';
+
+export class StationersGame {
+  round: number;
+
+  constructor() {
+    this.round = 0;
+  }
+}
--- frontend/src/routes/game/+page.svelte
+++ frontend/src/routes/game/+page.svelte
@@ -1,4 +1,15 @@
-<div class="flex flex-col">
-  <h1 class="text-xl font-bold">Stationers</h1>
-  <p>Testing interface</p>
+<script lang="ts">
+  import Button from '@/components/ui/button/button.svelte';
+  import { StationersGame } from '@/game/game.svelte';
+  let game = new StationersGame();
+</script>
+
+<div class="flex flex-col items-start gap-2">
+  <div>
+    <h1 class="text-xl font-bold">Stationers</h1>
+    <p>Testing interface</p>
+  </div>
+
+  <p>Round: {game.round}</p>
+  <Button onclick={() => game.round++}>click me</Button>
 </div>
